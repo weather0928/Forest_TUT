a218b712ff9fe619e23b52023ac8a8e268636cc0
tenki teramura <m0118187ab@edu.teu.ac.jp>
2020-06-24 15:47:35 +0900

    商人の音判断処理範囲のバグ調整
    商人の移動に合わせて音判断の範囲が動いていなかったバグを発見したので修正

---
+++ Assets/Script/PlayerSearchArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSearchArea : MonoBehaviour
+{
+
+    [SerializeField] GameObject Enemy;
+
+    void OnTriggerStay(Collider other) //ターゲット（プレイヤー）索敵処理
+    {
+        if (other.gameObject.tag == "Player" && Physics.Linecast(Enemy.transform.position + Vector3.up, other.transform.position + Vector3.up) == false)
+        {
+            EnemyChaser.inArea = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other) //索敵範囲からターゲットが出たときにする処理
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            EnemyChaser.chaseSwitchFlag = true;
+        }
+    }
+}
