88d44a272c51a0b6417676dacc5228f6f9561991
tenki teramura <m0118187ab@edu.teu.ac.jp>
2020-07-05 16:59:04 +0900

    完成
    ハンマーを持ってるときは制作時間が1/2になる仕様も実装済み
    あと必要かと思って制作中にゲージを出して、完成まであとどれくらいかかるかをわかるようにしました

--- Assets/Script/CreateItem.cs
+++ Assets/Script/CreateItem.cs
@@ -1,39 +1,93 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Linq;
 
 public class CreateItem : MonoBehaviour
 {
     [System.NonSerialized] public static Item craftItemDate;
     [SerializeField] private ItemManeger itemManeger;
+    [SerializeField] Item hammerItem;
+    [SerializeField] int hammerUse;
     [SerializeField] private float stopTime;
     [SerializeField] private Color origColor;
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject slider;
     private float seconds;
+    int hammerRemainingUses;
     [System.NonSerialized] public static bool craftFlag;
+    [System.NonSerialized] public static bool createStartFlag;
+
+    private void Start()
+    {
+        createStartFlag = false;
+        slider.SetActive(false);
+    }
 
     private void Update()
     {
         if(craftFlag == true)
         {
+            if(createStartFlag == true)
+            {
+                slider.SetActive(true);
+                if(itemManeger.numOfItem[hammerItem] == 1)
+                {
+                    slider.GetComponent<Slider>().maxValue = stopTime / 2;
+                }
+                else
+                {
+                    slider.GetComponent<Slider>().maxValue = stopTime;
+                }
+                createStartFlag = false;
+            }
+            slider.GetComponent<Slider>().value = seconds;
             SoundJudge.soundFlag = true;
             SoundJudge.soundPoint = player.transform.position;
             seconds += Time.deltaTime;
             player.GetComponent<Renderer>().material.color = new Color(255f / 255f, 65f / 255f, 26f / 255f, 255f / 255f);
             Debug.Log(seconds);
-            if (seconds >= stopTime)
+            this.GetComponent<CanvasGroup>().alpha = 0;
+            this.GetComponent<CanvasGroup>().interactable = false;
+            if(itemManeger.numOfItem[hammerItem] == 1)
             {
-                itemManeger.numOfItem[craftItemDate] += 1;
-                craftFlag = false;
-                SoundJudge.soundFlag = false;
-                seconds = 0.0f;
-                //PlayerMove.moveFlag = true;
-                Debug.Log(itemManeger.numOfItem[craftItemDate]);
-                player.GetComponent<Renderer>().material.color = origColor;
+                if (seconds >= stopTime / 2)
+                {
+                    CreateComplete();
+                    hammerRemainingUses--;
+                    if(hammerRemainingUses == 0)
+                    {
+                        itemManeger.numOfItem[hammerItem]--;
+                    }
+                }
+            }
+            else
+            {
+                if (seconds >= stopTime)
+                {
+                    CreateComplete();
+                }
             }
         }
         
     }
- }
+
+    private void CreateComplete()
+    {
+        itemManeger.numOfItem[craftItemDate] += 1;
+        if(craftItemDate == hammerItem)
+        {
+            hammerRemainingUses = hammerUse;
+        }
+        craftFlag = false;
+        SoundJudge.soundFlag = false;
+        seconds = 0.0f;
+        this.GetComponent<CanvasGroup>().alpha = 1;
+        this.GetComponent<CanvasGroup>().interactable = true;
+        Debug.Log(itemManeger.numOfItem[craftItemDate]);
+        player.GetComponent<Renderer>().material.color = origColor;
+        slider.SetActive(false);
+    }
+}
 
--- Assets/Script/CreateItemButton.cs
+++ Assets/Script/CreateItemButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Linq;
 
 public class CreateItemButton : MonoBehaviour
@@ -15,6 +16,8 @@ public class CreateItemButton : MonoBehaviour
     }
     [SerializeField] private List<Cost> craftCost = new List<Cost>();
     private bool[] conditionFlag;
+
+    [SerializeField] bool hammerFlag;
 
     private void Start()
     {
@@ -34,6 +37,29 @@ public class CreateItemButton : MonoBehaviour
                 conditionFlag[i] = true;
             }
         }
+
+        if(hammerFlag == true)
+        {
+            if (conditionFlag.All(i => i == true) && itemManeger.numOfItem[itemDate] == 0)
+            {
+                this.GetComponent<Button>().interactable = true;
+            }
+            else if (conditionFlag.All(i => i == false) || itemManeger.numOfItem[itemDate] == 1)
+            {
+                this.GetComponent<Button>().interactable = false;
+            }
+        }
+        else
+        {
+            if (conditionFlag.All(i => i == true))
+            {
+                this.GetComponent<Button>().interactable = true;
+            }
+            else if (conditionFlag.All(i => i == false))
+            {
+                this.GetComponent<Button>().interactable = false;
+            }
+        }
     }
 
     public void OnClick()
@@ -41,6 +67,8 @@ public class CreateItemButton : MonoBehaviour
         if (conditionFlag.All(i => i == true) && CreateItem.craftFlag == false)
         {
             CreateItem.craftFlag = true;
+            CreateItem.craftItemDate = itemDate;
+            CreateItem.createStartFlag = true;
             for (int i = 0; i < craftCost.Count; i++)
             {
                 itemManeger.numOfItem[craftCost[i].MaterialItem] -= craftCost[i].amount;
