d7f68a4ac4fe85be8a9f37e2cd90e998719d8332
tenki teramura <m0118187ab@edu.teu.ac.jp>
2020-07-22 07:21:40 +0900

    ポーズ&ゲームUI修正
    新たな仕様に修正

--- Assets/Script/UseItemUI.cs
+++ Assets/Script/UseItemUI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Linq;
 
 public class UseItemUI : MonoBehaviour
@@ -16,10 +17,24 @@ public class UseItemUI : MonoBehaviour
     [SerializeField] GameObject bellUI;
     [SerializeField] Item bellDate;
     [SerializeField] GameObject trapUI;
+    [SerializeField] Item trapDate;
     private bool[] itemFlag;
     private int itemNumber;
     private bool switchFlag;
     private bool switchConpleateFlag;
+    private LadderSet ladderSet;
+    private ThrowItem throwItem;
+    private StaleItemSet staleItemSet;
+
+    private void Start()
+    {
+        ladderSet = ladderUI.GetComponent<LadderSet>();
+        throwItem = bellUI.GetComponent<ThrowItem>();
+        staleItemSet = trapUI.GetComponent<StaleItemSet>();
+        ladderUI.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        bellUI.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        trapUI.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+    }
 
     // Update is called once per frame
     void Update()
@@ -29,21 +44,34 @@ public class UseItemUI : MonoBehaviour
             return;
         }
         
-        
+        if(itemManeger.numOfItem[ladderDate] >= 1 && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ImageAlpha(ladderUI, bellUI, trapUI);
+            ladderSet.enabled = true;
+            throwItem.enabled = false;
+            staleItemSet.enabled = false;
+        }
+        else if(itemManeger.numOfItem[bellDate] >= 1 && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ImageAlpha(bellUI,ladderUI, trapUI);
+            ladderSet.enabled = false;
+            throwItem.enabled = true;
+            staleItemSet.enabled = false;
+        }
+        else if(itemManeger.numOfItem[trapDate] >= 1 && Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ImageAlpha(trapUI,ladderUI,bellUI);
+            ladderSet.enabled = false;
+            throwItem.enabled = false;
+            staleItemSet.enabled = true;
+        }
         
     }
 
-    private bool ItemCheck(Item item)
+    private void ImageAlpha(GameObject useItem,GameObject nonUseItem1,GameObject nonUseItem2)
     {
-        bool itemFlag;
-        if (itemManeger.numOfItem[item] <= 0)
-        {
-            itemFlag = false;
-        }
-        else
-        {
-            itemFlag = true;
-        }
-        return itemFlag;
+        useItem.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        nonUseItem1.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        nonUseItem2.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
     }
 }
