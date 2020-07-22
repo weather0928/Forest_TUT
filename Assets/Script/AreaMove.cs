6568f1f945722b9fe968999fd45d92872828556a
tenki teramura <m0118187ab@edu.teu.ac.jp>
2020-07-22 16:00:14 +0900

    途中保存
    終わらねぇぇぇぇぇぇぇぇぇ

---
+++ Assets/Script/AreaMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaMove : MonoBehaviour
+{
+
+    [SerializeField] Transform newAreaPoint;
+    [SerializeField] Transform oldAreaPoint;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject enemy;
+
+    bool areaMoveFlag;
+    bool enemyMoveFlag;
+    float enemyMoveTime = 5.0f;
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemyMoveFlag == true)
+        {
+            time += Time.deltaTime;
+            Debug.Log(time);
+            if (time >= enemyMoveTime)
+            {
+                enemy.SetActive(true);
+                time = 0;
+                enemyMoveFlag = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == player)
+        {
+            player.transform.position = newAreaPoint.position;
+            enemyMoveFlag = true;
+            time = 0;
+        }
+    }
+}
